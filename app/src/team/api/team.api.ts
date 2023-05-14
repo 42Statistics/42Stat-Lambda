59bea17e7c85b06017865cdee0f8d71baceaa61b
jaham <jpham0005@gmail.com>
2023-05-14 17:57:31 +0900

    feat: :sparkles: team 상태에 따른 type 추가

--- app/src/team/api/team.api.ts
+++ app/src/team/api/team.api.ts
@@ -1,9 +1,29 @@
 import { z } from 'zod';
 import { FT_CURSUS_ID } from '../../cursusUser/api/cursusUser.api.js';
 import { parseFromDtoMany } from '../../util/parseFromDto.js';
-import { teamSchema, teamSchema_ } from './team.schema.js';
+import { teamBaseSchema, teamSchema, teamSchema_ } from './team.schema.js';
 
+export type TeamBase = z.infer<typeof teamBaseSchema>;
 export type Team = z.infer<typeof teamSchema>;
+export type ValidatedTeam = Omit<
+  Team,
+  'finalMark' | 'locked?' | 'validated?' | 'lockedAt'
+> & {
+  finalMark: number;
+  'locked?': true;
+  'validated?': boolean;
+  lockedAt: Date;
+};
+
+export type PassedTeam = Omit<
+  Team,
+  'finalMark' | 'locked?' | 'validated?' | 'lockedAt'
+> & {
+  finalMark: number;
+  'locked?': true;
+  'validated?': true;
+  lockedAt: Date;
+};
 
 const UPDATED = (start: Date, end: Date): string =>
   `https://api.intra.42.fr/v2/cursus/${FT_CURSUS_ID}/teams?filter[campus]=29&range[updated_at]=${start.toISOString()},${end.toISOString()}&sort=created_at`;
@@ -14,3 +34,11 @@ export const TEAM_EP = {
 
 export const parseTeams = (dtos: object[]): z.infer<typeof teamSchema_>[] =>
   parseFromDtoMany(dtos, teamSchema_, 'teams');
+
+export const isValidatedTeam = (team: TeamBase): team is ValidatedTeam => {
+  return team['validated?'] !== null;
+};
+
+export const isPassedTeam = (team: TeamBase): team is PassedTeam => {
+  return team['validated?'] === true;
+};
