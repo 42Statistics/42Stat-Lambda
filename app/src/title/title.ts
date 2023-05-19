c78440f03b6b4b5288035f1164cbb0b128fa745d
jaham <jpham0005@gmail.com>
2023-05-19 16:39:22 +0900

    fix: :bug: title, tiltes user api 같이 정렬도 안되는 api 들을 전부 받아오도록 임시 처리
    일단 전부 받아오는 방식으로 고쳤는데, (다른 방법이 없음) 이로 인해 시간이 너무 오래 걸리는 문제가 있습니다. 이를 해결하기 위해 다른 lambda 함수를 만드는 것도 고려해야할 것 같네요.

--- app/src/title/title.ts
+++ app/src/title/title.ts
@@ -1,13 +1,12 @@
 import { MongoClient } from 'mongodb';
+import { upsertManyById } from '../mongodb/mongodb.js';
 import {
   FetchApiAction,
-  LogAsyncDocumentCount,
   LogAsyncEstimatedTime,
   UpdateAction,
 } from '../util/decorator.js';
-import { getDocuemntCount, upsertManyById } from '../mongodb/mongodb.js';
-import { TITLE_EP, Title, parseTitles } from './api/title.api.js';
 import { pagedRequestByCount } from '../util/pagedRequestByCount.js';
+import { TITLE_EP, Title, parseTitles } from './api/title.api.js';
 
 export const TITLE_COLLECTION = 'titles';
 
@@ -16,39 +15,33 @@ export class TitleUpdator {
   /**
    *
    * @description
-   * @see updateFromLastPage   L: title 전체
+   * @see updateAll   L: title 전체
    *
    * 2023-05 기준
-   * 필요 요청 수: L(1)
-   * 예상 소요 시간: 2초
+   * 필요 요청 수: L(14)
+   * 예상 소요 시간: 20초 이상
    *
    * 아무런 인자를 넘길 수 없기 때문에, 테스트 요청을 하나 보내어 받아와야 함.
+   * 응답에 규칙성이 없기 때문에 처음부터 다 받아와야함.
    * 요청 수는 칭호가 한번에 100개 이상 생기지 않는 이상 불변함.
    */
   static async update(mongoClient: MongoClient): Promise<void> {
-    await this.updateFromLastPage(mongoClient);
+    await this.updateAll(mongoClient);
   }
 
   @UpdateAction
   @LogAsyncEstimatedTime
-  private static async updateFromLastPage(
-    mongoClient: MongoClient,
-  ): Promise<void> {
-    const documentCount = await getDocuemntCount(mongoClient, TITLE_COLLECTION);
-
-    const titles = await this.fetchFromLastPage(documentCount);
+  private static async updateAll(mongoClient: MongoClient): Promise<void> {
+    const titles = await this.fetchAll();
 
     await upsertManyById(mongoClient, TITLE_COLLECTION, titles);
   }
 
   @FetchApiAction
-  @LogAsyncDocumentCount
-  private static async fetchFromLastPage(
-    documentCount: number,
-  ): Promise<Title[]> {
+  private static async fetchAll(): Promise<Title[]> {
     const titlesUserDtos = await pagedRequestByCount(
       TITLE_EP.FROM_LAST_PAGE(),
-      documentCount,
+      0,
       100,
     );
 
--- app/src/titlesUser/titlesUser.ts
+++ app/src/titlesUser/titlesUser.ts
@@ -1,18 +1,17 @@
 import { MongoClient } from 'mongodb';
-import { getDocuemntCount, upsertManyById } from '../mongodb/mongodb.js';
+import { upsertManyById } from '../mongodb/mongodb.js';
 import {
   FetchApiAction,
-  LogAsyncDocumentCount,
   LogAsyncEstimatedTime,
   UpdateAction,
 } from '../util/decorator.js';
-import { pagedRequestByCount } from '../util/pagedRequestByCount.js';
+import { pagedRequest } from '../util/pagedRequest.js';
 import {
   TITLES_USER_EP,
   TitlesUser,
   parseTitlesUsers,
 } from './api/titlesUser.api.js';
-
+import { pagedRequestByCount } from '../util/pagedRequestByCount.js';
 export const TITLES_USER_COLLECTION = 'titles_users';
 
 // eslint-disable-next-line
@@ -20,43 +19,33 @@ export class TitlesUserUpdator {
   /**
    *
    * @description
-   * @see updateFromLastPage   L: 전체 titlesUser
+   * @see updateAll   L: 전체 titlesUser
    *
    * 2023-05 기준
-   * 필요 요청 수: L(1)
-   * 예상 소요 시간: 2초
+   * 필요 요청 수: L(150 ~ 200)
+   * 예상 소요 시간: 3분 이상
    *
    * 아무런 인자를 넘길 수 없기 때문에, 테스트 요청을 하나 보내어 받아와야 함.
-   * 요청 수 자체는 불변할 것으로 예상되나, writer's soul 처럼 나중에 생긴 칭호를 일괄 지급하는 경우,
-   * 증가할 가능성이 있음.
+   * 응답에 아무런 규칙성이 없기 때문에 처음부터 다 받아와야함.
+   * 선형적으로 이 시간이 증가할텐데, 마땅한 대책이 없음. 별도의 람다 함수로 분리하는 것도 고려해야할 듯.
    */
   static async update(mongoClient: MongoClient): Promise<void> {
-    await this.updateFromLastPage(mongoClient);
+    await this.updateAll(mongoClient);
   }
 
   @UpdateAction
   @LogAsyncEstimatedTime
-  private static async updateFromLastPage(
-    mongoClient: MongoClient,
-  ): Promise<void> {
-    const docCount = await getDocuemntCount(
-      mongoClient,
-      TITLES_USER_COLLECTION,
-    );
-
-    const titlesUsers = await this.fetchFromLastPage(docCount);
+  private static async updateAll(mongoClient: MongoClient): Promise<void> {
+    const titlesUsers = await this.fetchAll();
 
     await upsertManyById(mongoClient, TITLES_USER_COLLECTION, titlesUsers);
   }
 
   @FetchApiAction
-  @LogAsyncDocumentCount
-  private static async fetchFromLastPage(
-    docCount: number,
-  ): Promise<TitlesUser[]> {
+  private static async fetchAll(): Promise<TitlesUser[]> {
     const titlesUserDtos = await pagedRequestByCount(
       TITLES_USER_EP.FROM_LAST_PAGE(),
-      docCount,
+      0,
       100,
     );
 
