e079bddb0f6daaee6b6ec9b6452ba686ed0c724d
jaham <jpham0005@gmail.com>
2023-10-23 20:35:21 +0900

    refactor: :recycle: ft client secret 을 aws secrets manager 에서 받아오도록 변경
    - close #51

--- app/src/request/initSeine.ts
+++ app/src/request/initSeine.ts
@@ -1,9 +1,32 @@
 import { assertEnvExist } from '#lambda/util/envCheck.js';
+import {
+  GetSecretValueCommand,
+  SecretsManagerClient,
+} from '@aws-sdk/client-secrets-manager';
 import seine from 'la-seine';
 
 export const initSeine = async (): Promise<void> => {
   const clientId = process.env.API_CLIENT_ID;
-  const clientSecret = process.env.API_CLIENT_SECRET;
+
+  const clientSecretId = process.env.API_CLIENT_SECRET_ID;
+  assertEnvExist(clientSecretId);
+
+  const secretsManagerClient = new SecretsManagerClient();
+  const clientSecretJsonString = await secretsManagerClient.send(
+    new GetSecretValueCommand({
+      SecretId: clientSecretId,
+    }),
+  );
+  secretsManagerClient.destroy();
+
+  assertEnvExist(clientSecretJsonString.SecretString);
+
+  const clientSecretKey = process.env.API_CLIENT_SECRET_KEY;
+  assertEnvExist(clientSecretKey);
+
+  const clientSecret = JSON.parse(clientSecretJsonString.SecretString)[
+    clientSecretKey
+  ];
 
   assertEnvExist(clientId);
   assertEnvExist(clientSecret);
