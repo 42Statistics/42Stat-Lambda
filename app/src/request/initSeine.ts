66031be1bbecef90caae319cf424436129184ca9
jaham <jpham0005@gmail.com>
2023-11-06 19:19:55 +0900

    refactor: :recycle: initSeine implicit any type 수정 및 함수 분리

--- app/src/request/initSeine.ts
+++ app/src/request/initSeine.ts
@@ -1,35 +1,66 @@
 import { assertEnvExist } from '#lambda/util/envCheck.js';
+import { LambdaError } from '#lambda/util/error.js';
 import {
   GetSecretValueCommand,
+  type GetSecretValueCommandOutput,
   SecretsManagerClient,
 } from '@aws-sdk/client-secrets-manager';
 import seine from 'la-seine';
 
 export const initSeine = async (): Promise<void> => {
   const clientId = process.env.API_CLIENT_ID;
+  assertEnvExist(clientId);
 
-  const clientSecretId = process.env.API_CLIENT_SECRET_ID;
-  assertEnvExist(clientSecretId);
+  const clientSecret = await getClientSecretFromAws();
 
+  await seine.updateApiClient({ clientId, clientSecret });
+};
+
+const fetchClientSecretFromAws = async (
+  clientSecretId: string,
+): Promise<GetSecretValueCommandOutput> => {
   const secretsManagerClient = new SecretsManagerClient();
   const clientSecretJsonString = await secretsManagerClient.send(
     new GetSecretValueCommand({
       SecretId: clientSecretId,
     }),
   );
+
   secretsManagerClient.destroy();
 
+  return clientSecretJsonString;
+};
+
+const awsSecretConfigErrorMessage = 'wrong aws secret config';
+const getClientSecretFromAws = async (): Promise<string> => {
+  const clientSecretId = process.env.API_CLIENT_SECRET_ID;
+  assertEnvExist(clientSecretId);
+
+  const clientSecretJsonString = await fetchClientSecretFromAws(clientSecretId);
   assertEnvExist(clientSecretJsonString.SecretString);
+
+  const clientSecretJson = JSON.parse(
+    clientSecretJsonString.SecretString,
+  ) as unknown;
 
   const clientSecretKey = process.env.API_CLIENT_SECRET_KEY;
   assertEnvExist(clientSecretKey);
 
-  const clientSecret = JSON.parse(clientSecretJsonString.SecretString)[
+  if (
+    !clientSecretJson ||
+    typeof clientSecretJson !== 'object' ||
+    !(clientSecretKey in clientSecretJson)
+  ) {
+    throw new LambdaError(awsSecretConfigErrorMessage);
+  }
+
+  const clientSecret = (clientSecretJson as Record<string, string>)[
     clientSecretKey
-  ];
+  ] as unknown;
 
-  assertEnvExist(clientId);
-  assertEnvExist(clientSecret);
+  if (!clientSecret || typeof clientSecret !== 'string') {
+    throw new LambdaError(awsSecretConfigErrorMessage);
+  }
 
-  await seine.updateApiClient({ clientId, clientSecret });
+  return clientSecret;
 };
